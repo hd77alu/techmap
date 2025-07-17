ee1fe2e9a79723b27569eb9aa0f767850c16920b
JesseWK100 <j.walusansa@alustudent.com>
2025-07-17 13:38:55 +0200

    updated the trendsRoutes.js

--- Backend/routes/trendsRoutes.js
+++ Backend/routes/trendsRoutes.js
@@ -2,5 +2,7 @@ const router = require('express').Router();
 const { ensureAuth } = require('../middleware/authMiddleware');
 const { getTrends } = require('../controllers/trendsController');
 
-router.get('/', ensureAuth, getTrends);
-module.exports = router;
+router.get('/', ensureAuth, async (req, res) => {
+  const data = await Trend.getAll();
+  res.json(data);
+});
